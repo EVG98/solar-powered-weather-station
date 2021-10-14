0518193351deb3c0b75740de43af9883147df0c8
Clemeit <tboulan@fastmail.com>
2021-10-14 12:09:07 -0700

    DS18B20 test sketch

--- DS18B20_test/DS18B20_test.ino
+++ DS18B20_test/DS18B20_test.ino
@@ -1,9 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+// Firmware test for DS18B20 Temperature Sensor
+// Connect the DS18B20 to 5V and GND
+// Connect the data pin to D2 on the Arduino
+// Connect a 4.7k pullup resistor to the data line
 
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include "TimerOne.h"
+
+float lastKnownTemperature;
+
+OneWire oneWire(2);
+DallasTemperature sensor(&oneWire);
+
+void setup() {
+  sensor.begin();
+  Serial.begin(9600);
+
+  Timer1.initialize(3000000);
+  Timer1.attachInterrupt(readTemperature);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+}
 
+void readTemperature() {
+  sensor.requestTemperatures();
+
+  lastKnownTemperature = sensor.getTempCByIndex(0) * 1.8 + 32;
+
+  Serial.print("Temperature: ");
+  Serial.print(lastKnownTemperature);
+  Serial.println(" Fahrenheit");
 }
